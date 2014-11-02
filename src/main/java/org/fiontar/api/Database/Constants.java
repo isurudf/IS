a1b127ebdbc420d752c46b94f0c9fccbf4d7bce9
Danula Eranjith <hmdanulae@gmail.com>
2014-11-02 22:17:50 +0530

    DB constants changed

--- src/main/java/org/fiontar/api/Database/Constants.java
+++ src/main/java/org/fiontar/api/Database/Constants.java
@@ -9,13 +9,13 @@ public class Constants {
 
     public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
     
-    /*public static final String DB_URL = "jdbc:mysql://us-cdbr-east-05.cleardb.net:3306/heroku_bbb7cce486a618f?reconnect=true";
-    public static final String DB_USERNAME = "bc034aaf56e885";
-    public static final String DB_PASSWORD = "0ee89db7";*/
+    public static final String DB_URL = "jdbc:mysql://us-cdbr-iron-east-01.cleardb.net/heroku_a0ab4bfcf7bb090?reconnect=true"
+    public static final String DB_USERNAME = "b6463ce19bcc65";
+    public static final String DB_PASSWORD = "25c07b4f";
     
-    public static final String DB_URL = "jdbc:mysql://localhost/is";
+  /*  public static final String DB_URL = "jdbc:mysql://localhost/is";
     public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "";
+    public static final String DB_PASSWORD = "123";*/
     
 /*    public static final String DB_URL = "jdbc:mysql://192.185.41.209:3306/slmaths_id";
     public static final String DB_USERNAME = "slmaths_isuru";
